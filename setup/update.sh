494b3d9d4431c4cadba2bd2435a800bade3f9671
aNz <107313945+anzclan@users.noreply.github.com>
2022-11-12 18:10:07 +0800

    Update update.sh

--- setup/update.sh
+++ setup/update.sh
@@ -7,8 +7,8 @@ export MENUPATH="/usr/local/sbin/"
 # TESTING
 echo -e "${THEME}│${NC} $INFO Run Auto Update..."
 wget -q -O ${MENUPATH}menu "${SCURL}/menu.sh" && chmod +x ${MENUPATH}menu
-wget -q -O ${MENUPATH}cleanlog "${SCURL}/cleanlog.sh" && chmod +x ${MENUPATH}cleanlog
-wget -q -O ${MENUPATH}expiry "${SCURL}/expiry.sh" && chmod +x ${MENUPATH}expiry
+wget -q -O ${MENUPATH}cleanbass "${SCURL}/cleanbass.sh" && chmod +x ${MENUPATH}cleanbass
+wget -q -O ${MENUPATH}xp "${SCURL}/xp.sh" && chmod +x ${MENUPATH}xp
 rm /etc/crontab
 wget -q -O /etc/crontab https://raw.githubusercontent.com/anzclan/XRAY-XTLS-WSS-TCP-MULTI/main/setup/menu/crontab
 systemctl restart cron
