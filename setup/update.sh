cc677fe65c1f8a8fa86d78118b6906c6a49b16f7
aNz <107313945+anzclan@users.noreply.github.com>
2022-11-13 07:32:13 +0800

    Update update.sh

--- setup/update.sh
+++ setup/update.sh
@@ -7,6 +7,12 @@ export MENUPATH="/usr/local/sbin/"
 # TESTING
 echo -e "${THEME}│${NC} $INFO Run Auto Update..."
 wget -q -O ${MENUPATH}menu "${SCURL}/menu.sh" && chmod +x ${MENUPATH}menu
+wget -q -O ${MENUPATH}menu-go "${SCURL}/menu-go.sh" && chmod +x ${MENUPATH}menu-go
+wget -q -O ${MENUPATH}menu-trojan "${SCURL}/menu-trojan.sh" && chmod +x ${MENUPATH}menu-trojan
+wget -q -O ${MENUPATH}menu-vless "${SCURL}/menu-vless.sh" && chmod +x ${MENUPATH}menu-vless
+wget -q -O ${MENUPATH}menu-vmess "${SCURL}/menu-vmess.sh" && chmod +x ${MENUPATH}menu-vmess
+wget -q -O ${MENUPATH}menu-xtrojan "${SCURL}/menu-xtrojan.sh" && chmod +x ${MENUPATH}menu-xtrojan
+wget -q -O ${MENUPATH}menu-xvless "${SCURL}/menu-xvless.sh" && chmod +x ${MENUPATH}menu-xvless
 wget -q -O ${MENUPATH}cleanbass "${SCURL}/cleanbass.sh" && chmod +x ${MENUPATH}cleanbass
 wget -q -O ${MENUPATH}xp "${SCURL}/xp.sh" && chmod +x ${MENUPATH}xp
 rm /etc/crontab
