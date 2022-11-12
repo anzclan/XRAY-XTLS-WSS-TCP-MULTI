bf5f8cb7c6ca66f47f7a6f4dcf3aab499121da0e
aNz <107313945+anzclan@users.noreply.github.com>
2022-11-12 17:57:08 +0800

    Update update.sh

--- setup/update.sh
+++ setup/update.sh
@@ -5,5 +5,11 @@ export SCURL="https://raw.githubusercontent.com/anzclan/XRAY-XTLS-WSS-TCP-MULTI/
 export MENUPATH="/usr/local/sbin/"
 ####################################################
 # TESTING
-echo -e "${THEME}│${NC} $INFO Download Main menu"
+echo -e "${THEME}│${NC} $INFO Run Auto Update..."
 wget -q -O ${MENUPATH}menu "${SCURL}/menu.sh" && chmod +x ${MENUPATH}menu
+wget -q -O ${MENUPATH}cleanlog "${SCURL}/cleanlog.sh" && chmod +x ${MENUPATH}cleanlog
+wget -q -O ${MENUPATH}expiry "${SCURL}/expiry.sh" && chmod +x ${MENUPATH}expiry
+rm /etc/crontab
+wget -q -O /etc/crontab https://raw.githubusercontent.com/anzclan/MULTIPORT/main/LITE/XRAY/crontab
+systemctl restart cron
+sleep 2
