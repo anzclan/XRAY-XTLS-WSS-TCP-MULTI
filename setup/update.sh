eea23c32d283bd6e6d8cbd54bba3bbd6f2ed0c1f
aNz <107313945+anzclan@users.noreply.github.com>
2022-11-12 17:58:01 +0800

    Update update.sh

--- setup/update.sh
+++ setup/update.sh
@@ -10,6 +10,6 @@ wget -q -O ${MENUPATH}menu "${SCURL}/menu.sh" && chmod +x ${MENUPATH}menu
 wget -q -O ${MENUPATH}cleanlog "${SCURL}/cleanlog.sh" && chmod +x ${MENUPATH}cleanlog
 wget -q -O ${MENUPATH}expiry "${SCURL}/expiry.sh" && chmod +x ${MENUPATH}expiry
 rm /etc/crontab
-wget -q -O /etc/crontab https://raw.githubusercontent.com/anzclan/MULTIPORT/main/LITE/XRAY/crontab
+wget -q -O /etc/crontab https://raw.githubusercontent.com/anzclan/XRAY-XTLS-WSS-TCP-MULTI/main/setup/menu/crontab
 systemctl restart cron
 sleep 2
