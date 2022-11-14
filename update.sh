85de0047891f80b38f01406ae7f99e61ca09dc9b
aNz <107313945+anzclan@users.noreply.github.com>
2022-11-14 18:24:52 +0800

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo -e "${THEME}│${NC} $INFO Download Update Package."
+sleep 2
+echo -e "${THEME}│${NC} $INFO Extract Update Package."
+sleep 2
+echo -e "${THEME}│${NC} $INFO Delete Unuse Package."
+sleep 2
